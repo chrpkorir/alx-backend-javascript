25a95026ceace2d5a06027ea7b3b1da73fa2ce03
Chrpkorir <faythkoko@gmail.com>
2022-12-23 11:46:51 +0000

    Resolve errors

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,11 +1,11 @@
-import ClassRoom from './0-classroom.js'
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
   const sizes = [19, 20, 34];
   const classArray = [];
 
   for (const size of sizes) {
-    classArray.push(new ClassRoom(sie));
+    classArray.push(new ClassRoom(size));
   }
   return classArray;
 }
